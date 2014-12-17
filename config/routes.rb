c13b4defed4e6079124651854847687e6fce96d7
Richa Arora <ricarora@gmail.com>
2014-12-17 12:09:41 -0800

    create hash through url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: "shipments#index"
+  get "/shipments/new", to: "shipments#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
