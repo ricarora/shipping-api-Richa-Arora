35db30f60c052182e55200f5af7f29e2a8bb312a
Richa Arora <ricarora@gmail.com>
2015-01-12 11:26:45 -0800

    add comments

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -5,7 +5,9 @@ class ShipmentsController < ApplicationController
 
   def create
     # format used: http://localhost:3000/shipments/new?[shipment]name=ri&[shipment]city=Seattle&[shipment]state=WA&[shipment]postal_code=98102&[shipment]weight=25
+
     @shipment = Shipment.new(shipment_params)
+    # We create a new log to be saved in our database to be able to audit later
     @log = Log.new
     @log.url = request.url
     @log.ip_address = request.ip
@@ -25,14 +27,17 @@ class ShipmentsController < ApplicationController
   private
 
   def shipment_params
+    # allowed params to pass as a shipment params
     params.require(:shipment).permit(:name, :city, :state, :postal_code, :weight)
   end
 
   def shipment_hash
+    # Only these value we are going to use
     {only: ["service_name", "total_price", "currency", "carrier"]}
   end
 
   def usps_result_hash(shipments_option)
+    # Converting USPS result into a standard response to be used in our api 
     shipments_option.collect do |shipment|
       { service_name: shipment.service_name,
         total_price: shipment.package_rates[0][:rate],
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,5 @@
 class Shipment < ActiveRecord::Base
+  # This is coming from the documentation of activeshipping gem
   include ActiveMerchant::Shipping
 
   validates :city, presence: true
@@ -8,33 +9,39 @@ class Shipment < ActiveRecord::Base
 
 
   def origin
+    # hard coding the origin location to be my home in Seattle, technically this
+    # could be the companies warehouse
     Location.new(country: "US", state: "WA", city: "Seattle", postal_code: "98112")
   end
 
   def destination
+    # We will get the destination location from our client and parse that hash to get this
     Location.new(country: "US", state: state, city: city, postal_code: postal_code)
   end
 
   def packages
-    Package.new(weight, [45.72, 35.56, 30.48], cylinder: false) #18" L 14" W 12" H. Input in cms, & grams
+    # We are hard coding the standard box size as 18" L 14" W 12" H
+    # We are using weight in grams and box dimensions in cms.
+    Package.new(weight, [45.72, 35.56, 30.48], cylinder: false)
   end
 
   def get_rates_from_shipper(shipper)
+    # Here we are quering the shipper api to get the rates and then sort it by
+    # rates (lowest to highest)
     response = shipper.find_rates(origin, destination, packages)
     response.rates.sort_by(&:price)
   end
 
   def ups_rates
+    # We are passing our access keys & password to the UPS client and then
+    # calling an api
     ups = UPS.new(login: ENV['UPS_ACCESS_KEY'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_ACCESS_KEY'])
     get_rates_from_shipper(ups)
   end
 
-  # def fedex_rates
-  #   fedex = FedEx.new(login: "your fedex login", password: "your fedex password", key: "your fedex key", account: "your fedex account number")
-  #   get_rates_from_shipper(fedex)
-  # end
-
   def usps_rates
+    # We are passing our access keys & password to the USPS client and then
+    # calling an api
     usps = USPS.new(login: ENV['USPS_USERNAME'], password: ENV['USPS_PASSWORD'])
     get_rates_from_shipper(usps)
   end
